932c08331f5ca6e823527dd804ddf42783db4519
DJones <dwaynejones@pursuit.com>
2023-12-13 04:19:59 -0500

    mentorship query completed 1o queries left to complete

--- queries/mentorship.js
+++ queries/mentorship.js
@@ -1,50 +1,67 @@
 const db = require('../db/dbConfig.js');
 
+// Retrieve all mentorships
 const getAllMentorships = async () => {
     try {
-
-    }catch(err) {
-        return err
+        const allMentorships = await db.any('SELECT * FROM mentorships');
+        return allMentorships;
+    } catch (err) {
+        return err;
     }
 };
 
-const getOneMentorship = async () => {
+// Retrieve a single mentorship by id
+const getOneMentorship = async (id) => {
     try {
-
-    }catch(err) {
-        return err
+        const oneMentorship = await db.one('SELECT * FROM mentorships WHERE id = $1', id);
+        return oneMentorship;
+    } catch (err) {
+        return err;
     }
 };
 
-const createMentorship = async () => {
+// Create a new mentorship
+const createMentorship = async (mentorship) => {
     try {
-
-    }catch(err) {
-        return err
+        const { mentor_id, mentee_id, start_date, end_date,status, notes } = mentorship;
+        const newMentorship = await db.one(
+            'INSERT INTO mentorships (mentor_id, mentee_id, start_date, end_date, status, notes) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+            [mentor_id, mentee_id, start_date, end_date,status, notes]
+        );
+        return newMentorship;
+    } catch (err) {
+        return err;
     }
 };
 
-const deleteMentorship = async () => {
+// Delete a mentorship
+const deleteMentorship = async (id) => {
     try {
-
-    }catch(err) {
-        return err
+        const deletedMentorship = await db.result('DELETE FROM mentorships WHERE id = $1', id);
+        return deletedMentorship;
+    } catch (err) {
+        return err;
     }
 };
 
-const updateMentorship = async () => {
+// Update a mentorship
+const updateMentorship = async (id, mentorship) => {
     try {
-
-    }catch(err) {
-        return err
+        const { mentor_id, mentee_id, start_date, end_date, status, notes } = mentorship;
+        const updatedMentorship = await db.one(
+            'UPDATE mentorships SET mentor_id=$1, mentee_id=$2, start_date=$3, end_date=$4, status=$5, notes=$6 WHERE id = $7 RETURNING *',
+            [mentor_id, mentee_id, start_date, end_date, status, notes, id]
+        );
+        return updatedMentorship;
+    } catch (err) {
+        return err;
     }
 };
 
-
-module.exports + {
+module.exports = {
     getAllMentorships,
     getOneMentorship,
     createMentorship,
     deleteMentorship,
     updateMentorship
-}
+};
--- queries/userConsent.js
+++ queries/userConsent.js
@@ -2,23 +2,28 @@ const db = require('../db/dbConfig.js');
 
 const getAllUserConsents = async () => {
     try {
-
+        console.log("Executing query to fetch all user consent");
+        const allUserConsentLogs = await db.any("SELECT * FROM user-consent_logs");
+        console.log("Query results:", allUserConsentLogs);
+        return allUserConsentLogs;
     } catch(err) {
         return err
     }
 };
 
-const getOneUserConsent = async () => {
+const getOneUserConsent = async (id) => {
     try {
-
+        const oneUserConsentLog = await  db.one("SELECT * FROM user_consent_logs");
+        return oneUserConsentLog
     } catch(err) {
         return err
     }
 };
 
-const createUserConsent = async () => {
+const createUserConsent = async (userConsent) => {
     try {
-
+        const createdUserConsent = await db.one("INSERT INTO user_consent_logs (user_id, document_id, consent_id, version) VALUES ($1, $2, $3, $4)RETURNING *" [userConsent.user_id, userConsent.document_id, userConsent.consent_date, userConsent.version]);
+        return createdUserConsent;
     } catch(err) {
         return err
     }
